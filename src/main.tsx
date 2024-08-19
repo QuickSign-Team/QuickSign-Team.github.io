5be832d9f76b4da86b9a58089022a8755b876405
BomberFish <bomberfish@bomberfish.ca>
2024-08-18 20:19:13 -0400

    add barebones 404

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,13 @@
 import 'dreamland/dev';
 import { Route, Router } from 'dreamland-router';
 import Home from './routes/home';
+import NotFound from './routes/not-found';
 
 //base styles
 import './index.css';
 new Router(
     <Route>
         <Route path="" show={<Home />} />
+        <Route path="*" show={<NotFound />} />
     </Route>
 ).mount(document.getElementById('app')!);
